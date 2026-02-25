d990878b7445283fd5da0ad9590b42b6c206afb8
Ali Mahdi <ali.mahdi.67.99@gmail.com>
2026-02-25 23:23:47 +0300

    test correct

--- nahrain-chat/src/main.jsx
+++ nahrain-chat/src/main.jsx
@@ -5,10 +5,9 @@ import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <HashRouter>
-    <StrictMode>
+  <StrictMode>
+    <HashRouter>
       <App />
-    </StrictMode>
-    ,
-  </HashRouter>,
+    </HashRouter>
+  </StrictMode>
 );
